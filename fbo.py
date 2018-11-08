ea89c79e06d719f7507180e3f50dedfca28bbeb9
Austin Peel <42186824+austinPeel0@users.noreply.github.com>
2018-11-08 13:13:19 -0500

    ENH add db stuff (#27)

--- fbo.py
+++ fbo.py
@@ -5,7 +5,7 @@ import json
 import os
 from utils import fbo_nightly_scraper as fbo, get_fbo_attachments
 from utils.predict import Predict 
-
+from utils.db import db
 
 def get_nightly_data(notice_types, naics):
     now = datetime.datetime.now() - datetime.timedelta(1)
@@ -39,7 +39,8 @@ def main(notice_types= ['MOD','PRESOL','COMBINE'], naics = {'336411','334419'}):
     predict = Predict(updated_nightly_data)
     updated_nightly_data = predict.insert_predictions()
     print("Done making predictions for each notice attachment!")
-
+    db.temp_remove_data_in_postgres()
+    db.DataAccessLayer().add_json_nightly_file_to_postgres(updated_nightly_data)  
     return updated_nightly_data, current_date
 
 if __name__ == '__main__':

3b022a976b8f4c811fd748ed54fff76912d3de94
csmcallister <scott.mcallister@gsa.gov>
2018-11-30 10:45:29 -0500

    FIX: update test for model table (#22)

--- test.py
+++ test.py
@@ -359,8 +359,8 @@ class PostgresTestCase(unittest.TestCase):
         self.assertEqual(trained_count,6)
     
     def test_model_insertion(self):
-        model = PostgresTestCase.db.query_model(estimator='estimator')
-        self.assertEqual(model[0],"estimator")
+        model = PostgresTestCase.db.query_model(estimator='SGDClassifier')
+        self.assertEqual(model[0],"SGDClassifier")
         
 if __name__ == '__main__':
     unittest.main()

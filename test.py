6a7eec7192556646cea1db40d5cc44a31671a937
csmcallister <scott.mcallister@gsa.gov>
2018-12-06 15:46:41 -0500

    FIX: hardcode dummy data

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 import unittest
-import importlib
 import os
 from utils.fbo_nightly_scraper import NightlyFBONotices
 from fixtures import nightly_file, json_str, filtered_json_str, nightly_data, updated_nightly_data
@@ -353,11 +352,90 @@ class PredictTestCase(unittest.TestCase):
 class PostgresTestCase(unittest.TestCase):
     
     def setUp(self):
-        dummy_data = importlib.import_module('fixtures.predicted_nightly_data')
-        predicted_nightly_data = dummy_data.predicted_nightly_data
-        predicted_nightly_data_day_two = dummy_data.predicted_nightly_data_day_two
         conn_string = get_db_url()
-        self.predicted_nightly_data = predicted_nightly_data
+        self.predicted_nightly_data = {'AMDCSS': [{'date': '0506',
+                                      'year': '18',
+                                      'agency': 'department of justice',
+                                      'office': 'federal bureau of investigation',
+                                      'location': 'procurement section',
+                                      'zip': '20535',
+                                      'classcod': '70',
+                                      'naics': '511210',
+                                      'offadd': '935 pennsylvania avenue, n.w. washington dc 20535',
+                                      'subject': 'enterprise business process management software tool',
+                                      'solnbr': 'rfp-e-bpm-djf-18-0800-pr-0000828',
+                                      'ntype': 'combine',
+                                      'contact': 'clark kent, contracting officer, phone 5555555555, email clark.kent@daily-planet.com',
+                                      'desc': '  link to document',
+                                      'url': 'url',
+                                      'setaside': 'n/a',
+                                      'popcountry': 'us',
+                                      'popzip': '20535',
+                                      'popaddress': '935 pennsylvania ave. n.w. washington, dc  ',
+                                      'attachments': [{'text': 'test_text_0',
+                                                       'url': 'test_url_0',
+                                                       'prediction': 1,
+                                                       'decision_boundary': 0,
+                                                       'validation': None,
+                                                       'trained': False},
+                                                       {'text': 'test_text_1',
+                                                       'url': 'test_url_1',
+                                                       'prediction': 1,
+                                                       'decision_boundary': 0,
+                                                       'validation': None,
+                                                       'trained': False},
+                                                       {'text': 'test_text_2',
+                                                       'url': 'test_url_2',
+                                                       'prediction': 1,
+                                                       'decision_boundary': 0,
+                                                       'validation': None,
+                                                       'trained': False},
+                                                       {'text': 'test_text_3',
+                                                       'url': 'test_url_3',
+                                                       'prediction': 1,
+                                                       'decision_boundary': 0,
+                                                       'validation': None,
+                                                       'trained': False},
+                                                       {'text': 'test_text_4',
+                                                       'url': 'test_url_4',
+                                                       'prediction': 1,
+                                                       'decision_boundary': 0,
+                                                       'validation': None,
+                                                       'trained': False},
+                                                       {'text': 'test_text_5',
+                                                       'url': 'test_url_5',
+                                                       'prediction': 1,
+                                                       'decision_boundary': 0,
+                                                       'validation': None,
+                                                       'trained': False},
+                                                       {'text': 'test_text_6',
+                                                       'url': 'test_url_6',
+                                                       'prediction': 1,
+                                                       'decision_boundary': 0,
+                                                       'validation': None,
+                                                       'trained': False}],
+                                      'compliant': 0}],
+                            'MOD': [],
+                            'COMBINE': [{'date': '0506',
+                                         'year': '18',
+                                         'agency': 'defense logistics agency',
+                                         'office': 'dla acquisition locations',
+                                         'location': 'dla aviation - bsm',
+                                         'zip': '23297',
+                                         'classcod': '66',
+                                         'naics': '334511',
+                                         'offadd': '334511',
+                                         'subject': 'subject',
+                                         'solnbr': 'spe4a618t934n',
+                                         'respdate': '051418',
+                                         'archdate': '06132018',
+                                         'contact': 'bob.dylan@aol.com',
+                                         'desc': 'test123',
+                                         'url': 'test_url',
+                                         'setaside': 'n/a  ',
+                                         'attachments': [],
+                                         'compliant': 0}],
+                            'PRESOL': []}
         self.predicted_nightly_data_copy = {'AMDCSS': [{'date': '0506',
                                                'year': '17',
                                                'agency': 'defense logistics agency',
@@ -462,8 +540,6 @@ class PostgresTestCase(unittest.TestCase):
         result = get_validation_count(self.dal)
         expected = 0
         self.assertEqual(result, expected)
-
-
 
 if __name__ == '__main__':
     unittest.main()

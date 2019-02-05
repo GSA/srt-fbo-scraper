2e3db68860c09c4e931e91fd27631525eefe0c25
csmcallister <scott.mcallister@gsa.gov>
2019-02-05 08:54:34 -0500

    TST: remove flag from supercronic call (#98)

--- test.py
+++ test.py
@@ -40,7 +40,7 @@ class SupercronicTestCase(unittest.TestCase):
         pass
 
     def test_supercronic_call(self):
-        process = subprocess.Popen(['supercronic', '-h'], stdout=subprocess.PIPE)
+        process = subprocess.Popen(['supercronic'], stdout=subprocess.PIPE)
         process.wait()
         result = process.returncode
         expected = 0

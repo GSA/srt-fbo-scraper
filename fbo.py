7092c8a28c9080e54a106ba56fe894dcc7ec55b4
csmcallister <scott.mcallister@gsa.gov>
2019-02-11 16:24:44 -0500

    LOG: stream logs to cf loggerator instead of file

--- fbo.py
+++ fbo.py
@@ -105,7 +105,7 @@ def main():
     logger.info("*"*80)
 
 if __name__ == '__main__':
-    logging.basicConfig(filename='fbo.log',
+    logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s') 
     main()
 

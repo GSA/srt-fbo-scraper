219d7f37232931d42a35f5abed94e58a2f43bc59
Al Crowley <albert.crowley@tcg.com>
2020-01-28 16:53:10 -0500

    Unit test fixes
    DB test setup change so tests will work properly with pre-populated DB
    Added check and test to make sure no notices are inserted with a null notice type
    No longer exit if there is an error downloading an attachment.
    Write logs to a smartie-logger.log file in addition to stdout
    Made the unit tests smarter about finding sample files in the fixtures directory.

--- tests/get_doc_text_test.py
+++ tests/get_doc_text_test.py
@@ -20,11 +20,13 @@ class GetDocTextTestCase(unittest.TestCase):
         if not os.path.exists(out_path):
             os.mkdir(out_path)
         self.abs_out_path = os.path.abspath(out_path)
-        
+
         # refer to fixture
-        if 'tests' not in os.getcwd():
-            self.temp_outfile_path_doc = os.path.join('tests', 'fixtures', 'test.doc')
-        else:
+        if 'fixtures' in os.getcwd():
+            self.temp_outfile_path_doc = os.path.join('test.doc')
+        elif '_trial_temp' in os.getcwd():  # running in pycharm
+            self.temp_outfile_path_doc = os.path.join('..', 'fixtures', 'test.doc')
+        elif 'tests' in os.getcwd():
             self.temp_outfile_path_doc = os.path.join('fixtures', 'test.doc')
 
         # create a mock file w/o extension

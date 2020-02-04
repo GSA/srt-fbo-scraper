2aa1ecd026b01983bdf9fa2eeeb3a31e6f7ea2e0
Al Crowley <albert.crowley@tcg.com>
2020-02-04 11:09:48 -0500

    Now log stats related to notice predictions.

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ dal.connect()
 
 def main():    
 
+    logger.info("Connecting with database at {}".format(conn_string))
     logger.info("Smartie is fetching opportunties from SAM...")
     data = get_opps.main()
     if not data:

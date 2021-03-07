b96ae9535c1a06f1030c59336f28b13faec18508
Al Crowley <albert.crowley@tcg.com>
2021-03-07 18:31:34 -0500

    Sprint 7 merge dev to staging (#266)
    * Merge branch 'dev' into staging
    
    # Conflicts:
    #	.circleci/config.yml
    #	crontab
    #	tests/db_test.py
    #	tests/sam_utils_test.py
    #	utils/db/db_utils.py
    #	utils/json_log_formatter.py
    #	utils/request_utils.py
    #	utils/sam_utils.py
    
    * adding na experiment notebook
    
    * working for a full run
    
    * adding Butch's train util class
    
    * Make sure to initialize b_text because some error condtions were leaving it unset causing errors later
    
    * Dev (#259)
    
    * adding na experiment notebook
    
    * working for a full run
    
    * adding Butch's train util class
    
    * Make sure to initialize b_text because some error condtions were leaving it unset causing errors later
    
    * triggering a new build after CI config updates
    
    * Adding bounds checking to schematize_opp  (#261)
    
    * Adding bounds checking to schematize_opp
    Adding check to make sure there are 2 levels of org hierarchy before trying to read the second level
    
    * fixed line removal
    
    * adding na experiment notebook
    
    * working for a full run
    
    * adding Butch's train util class
    
    * Make sure to initialize b_text because some error condtions were leaving it unset causing errors later
    
    * triggering a new build after CI config updates
    
    * Adding bounds checking to schematize_opp  (#261)
    
    * Adding bounds checking to schematize_opp
    Adding check to make sure there are 2 levels of org hierarchy before trying to read the second level
    
    * fixed line removal
    
    * turn off parallel builds. The overhead of the occasionaly slow container removes any advantage of the parallel testing
    
    * Process SAM datafeed to update solicitations that do not match filters (#265)
    
    * Adding bounds checking to schematize_opp
    Adding check to make sure there are 2 levels of org hierarchy before trying to read the second level
    
    * fixed line removal
    
    * Update the active flag in the solicitation table based on the SAM.gov solicitation datafeed
    
    * Adding Selenium and chrome drivers
    * Adding post-update step to review active predictions by solNumber to make sure they are still active
    
    * changing time of new solicitaiton fetch for testing
    
    * fixing datafeed download location to work with docker image
    
    * updated crontab for testing
    
    * Adding notice type updates via datafeed
    
    * Clear the Predictions materialized view when updating a notice based on the datafeed.
    * Also broke the update_old_solicitations function into manageable sub-functions

---
+++ tests/solicitaiton_update_test.py
@@ -0,0 +1,133 @@
+import os
+from time import sleep
+import sys
+import unittest
+from pathlib import Path
+from utils.db.db_utils import get_db_url, session_scope, DataAccessLayer, insert_data
+
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from utils.sam_utils import get_sol_data_from_feed, update_sam_data_feed, update_old_solicitations, \
+                            SAM_DATA_FEED_EXISTED, SAM_DATA_FEED_DOWNLOADED, SAM_DATA_FEED_ERROR
+
+import utils.db.db_utils
+from utils.db.db import Notice, Predictions, Solicitations
+
+
+class SamUtilsTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+    def test_update_sam_data_feed(self):
+        existing_filename = '/tmp/sam_data_feed.tmp'
+        Path(existing_filename).touch()
+
+        new_filename = '/tmp/ContractOpportunitiesFullCSV.csv'
+        if os.path.isfile(new_filename):
+            os.remove(new_filename)
+
+        assert update_sam_data_feed(existing_filename) == SAM_DATA_FEED_EXISTED
+
+        assert update_sam_data_feed(new_filename, 30) == SAM_DATA_FEED_DOWNLOADED
+        assert os.path.isfile(new_filename)
+        assert os.path.getsize(new_filename) > 400 * 1000 * 1000  # larger than 400 MB
+
+        assert update_sam_data_feed(new_filename, 30) == SAM_DATA_FEED_EXISTED
+
+    #
+    # def test_get_sol_data_from_feed(self):
+    #     sol_data = get_sol_data_from_feed("36C24121Q0028")
+    #     assert "SolNum" in sol_data
+    #     assert sol_data["SolNum"] == "36C24121Q0028"
+    #
+    # def test_update_from_sam_feed(self):
+    #
+    #     count = 0
+    #     # first remove all invalid marks
+    #     conn_string = get_db_url()
+    #     dal = DataAccessLayer(conn_string)
+    #     dal.connect()
+    #     with session_scope(dal) as session:
+    #         stmt = session.query(Solicitations).filter(Solicitations.active.is_(False)).update({"active" : True})
+    #         session.commit()
+    #
+    #         update_old_solicitations(session, age_cutoff=7, max_updates=200)
+    #
+    #         # we should now have a bunch of inactive solicitations
+    #         count = session.query(Solicitations).filter(Solicitations.active.is_(False)).count()
+    #
+    #     assert count > 0
+    #
+    #
+    # def test_update_from_sam_feed_date(self):
+    #
+    #     # first remove all invalid marks
+    #     conn_string = get_db_url()
+    #     dal = DataAccessLayer(conn_string)
+    #     dal.connect()
+    #     with session_scope(dal) as session:
+    #         stmt = session.query(Solicitations).filter(Solicitations.active.is_(False)).update({"active": True})
+    #         session.commit()
+    #
+    #         update_old_solicitations(session, age_cutoff=36500, max_updates=1000)
+    #
+    #         # There shouldn't be any solicitations that are 100 years old
+    #         count = session.query(Solicitations).filter(Solicitations.active.is_(False)).count()
+    #
+    #     assert count == 0
+    #
+    #
+    # def test_update_from_sam_feed_updatedAt(self):
+    #
+    #     conn_string = get_db_url()
+    #     dal = DataAccessLayer(conn_string)
+    #     dal.connect()
+    #     with session_scope(dal) as session:
+    #         # first remove all invalid marks
+    #         stmt = session.query(Solicitations).filter(Solicitations.active.is_(False)).update({"active": True})
+    #         session.commit()
+    #
+    #         before_pred = session.query(Predictions).order_by(Predictions.updatedAt).first()
+    #         before_date = before_pred.updatedAt
+    #
+    #         update_old_solicitations(session, age_cutoff=10, max_updates=200)
+    #
+    #         after_pred = session.query(Predictions).order_by(Predictions.updatedAt.desc()).first()
+    #         after_date = after_pred.updatedAt
+    #
+    #
+    #     print (before_pred)
+    #     print(after_pred)
+    #
+    #     assert after_date > before_date
+    #
+    # def test_stats_for_update(self):
+    #     max_updates = 500
+    #
+    #     conn_string = get_db_url()
+    #     dal = DataAccessLayer(conn_string)
+    #     dal.connect()
+    #     with session_scope(dal) as session:
+    #         # first remove all invalid marks
+    #         stmt = session.query(Solicitations).filter(Solicitations.active.is_(False)).update({"active": True})
+    #         session.commit()
+    #
+    #         stats = update_old_solicitations(session, age_cutoff=0, max_updates=max_updates)
+    #         all_predictions_examined = session.query(Predictions.id).count()
+    #         actual_updated_count = session.query(Solicitations.filter(Solicitations.active == False)).count()
+    #
+    #
+    #
+    #     assert stats['examined'] == max_updates+1
+    #     assert stats['examined'] < stats['updated']
+    #     assert actual_updated_count == stats['updated']
+    #
+
+
+if __name__ == '__main__':
+    unittest.main()
